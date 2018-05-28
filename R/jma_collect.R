2085758f4e374c4bf54308ff421a9e1aaa12cec9
Shinya Uryu <uryu.shinya@nies.go.jp>
2018-05-28 13:31:57 +0900

    Closed #1

--- R/jma_collect.R
+++ R/jma_collect.R
@@ -7,26 +7,25 @@
 #' @param day select date (default `NULL`)
 #' @examples
 #' \dontrun{
-#' jma_collect(item = "annually_a", "1284", year = 2017, month = 11, day = NULL)
+#' jma_collect(item = "annually", "1284", year = 2017, month = 11, day = NULL)
 #' # daily
-#' jma_collect(item = "daily_a1", block_no = "0010", year = 2017, month = 11, day = NULL)
-#' jma_collect(item = "daily_a1", "0422", year = 2017, month = 11, day = NULL)
+#' jma_collect(item = "daily", block_no = "0010", year = 2017, month = 11, day = NULL)
+#' jma_collect(item = "daily", "0422", year = 2017, month = 11, day = NULL)
 #' }
 #' @export
 jma_collect <- function(item = NULL,
                         block_no, year, month, day = NULL) {
 
   .blockid <- rlang::enquo(block_no)
-  x <-
-    xml2::read_html(
-      jma_url(item, !!.blockid, year, month, day)
-    )
+
+  target <-
+    jma_url(item, !!.blockid, year, month, day)
 
   df_raw <-
-    x %>%
+    xml2::read_html(target$url) %>%
     rvest::html_table(fill = TRUE)
 
-  if (item == "annually_a") {
+  if (item == "annually" & target$station_type == "a") {
     fix_names <-
       c("年",
         paste0("降水量_",
@@ -61,7 +60,7 @@ jma_collect <- function(item = NULL,
       convert_error(df) %>%
       dplyr::mutate_all(.funs = dplyr::funs(stringr::str_remove(., "]"))) %>%
       readr::type_convert(col_types = readr::cols(.default = readr::col_number()))
-  } else if (item == "monthly_a1") {
+  } else if (item == "monthly" & target$station_type == "a1") {
     df <-
       df_raw[[6]][-c(1:2), ]
 
@@ -84,7 +83,7 @@ jma_collect <- function(item = NULL,
       dplyr::mutate_all(.funs = dplyr::funs(stringr::str_remove(., "]"))) %>%
       readr::type_convert(col_types = readr::cols(.default = readr::col_number()))
 
-  } else if(item == "10daily_a1") {
+  } else if(item == "10daily" & target$station_type == "a1") {
 
     df <-
       df_raw[[6]][-c(1:2), ]
@@ -109,7 +108,7 @@ jma_collect <- function(item = NULL,
       dplyr::mutate_all(.funs = dplyr::funs(stringr::str_remove(., "]"))) %>%
       readr::type_convert(col_types = readr::cols(.default = readr::col_number()))
 
-  } else if (item == "daily_a1") {
+  } else if (item == "daily" & target$station_type == "a1") {
     df <-
       df_raw[[6]][-c(1:2), ]
 
@@ -133,7 +132,32 @@ jma_collect <- function(item = NULL,
                                   stringr::str_pad(month, width = 2, pad = "0"),
                                   stringr::str_pad(`日`, width = 2, pad = "0"), sep = "-"))) %>%
       readr::type_convert(col_types = readr::cols(.default = readr::col_number()))
-  } else if (item == "hourly_a1") {
+  } else if (item == "daily" & target$station_type == "s1") {
+    df <-
+      df_raw[[6]][-c(1:2), ]
+
+    fix_names <- c(
+      "日",
+      paste0("気圧_", c("現地平均", "海面平均"), "(hPa)"),
+      paste0("降水量_", c("合計", "最大1時間", "最大10分間"), "(mm)"),
+      paste0("気温_", c("平均", "最高", "最低"), "(\u2103)"),
+      paste0("湿度_", c("平均", "最小"), "(%)"),
+      paste0("風向・風速_",
+             c("平均風速", "最大風速風速", "最大風速風向",
+               "最大瞬間風速風速", "最大瞬間風速風向"), "(m/s)"),
+      paste0("日照時間_", "(h)"),
+      paste0("雪_", c("降雪合計", "最深積雪"), "(寒候年.cm)"),
+      paste0("天気概況", c("昼 (06:00-18:00)", "夜 (18:00-翌日06:00)"))
+    )
+
+    names(df) <- fix_names
+
+    df <-
+      convert_error(df) %>%
+      dplyr::mutate_all(.funs = dplyr::funs(stringr::str_remove(., "]"))) %>%
+      readr::type_convert(col_types = readr::cols(.default = readr::col_number()))
+
+  } else if (item == "hourly" & target$station_type == "a1") {
 
     df <-
       df_raw[[5]][-c(1:2), ]
@@ -145,7 +169,7 @@ jma_collect <- function(item = NULL,
       convert_error(df) %>%
       dplyr::mutate_all(.funs = dplyr::funs(stringr::str_remove(., "]"))) #%>%
       #readr::type_convert(col_types = readr::cols(.default = readr::col_number()))
-  } else if (item == "hourly_s1") {
+  } else if (item == "hourly" & target$station_type == "s1") {
     df <-
       df_raw[[5]][-c(1), ]
 
@@ -157,7 +181,7 @@ jma_collect <- function(item = NULL,
       dplyr::mutate_all(.funs = dplyr::funs(stringr::str_remove(., "]"))) %>%
       readr::type_convert()
 
-  } else if (item == "monthly_a1") {
+  } else if (item == "monthly" & target$station_type == "a1") {
     df <-
       df_raw[[6]][-c(1:2), ]
 
@@ -176,7 +200,7 @@ jma_collect <- function(item = NULL,
   df
 }
 
-#' jma_url(item = "annually_a", "0010", year = 2017, month = 11, day = NULL)
+#' jma_url(item = "annually", "0010", year = 2017, month = 11, day = NULL)
 jma_url <- function(item = NULL,
                     block_no, year, month, day = NULL) {
   .blockid <- rlang::enquo(block_no)
@@ -187,14 +211,28 @@ jma_url <- function(item = NULL,
     rlang::abort("この中から選択")
   }
 
-  pref <-
-    dplyr::filter(stations, block_no == !! .blockid)$prec_no
+  df_target_station <-
+    dplyr::filter(stations, block_no == !! rlang::eval_tidy(.blockid))
 
-  glue::glue(
-    "http://www.data.jma.go.jp/obd/stats/etrn/view/{selected_item}.php?prec_no={pref}&block_no={blockid}&year={year}&month={month}&day={day}&view=",
-    day = dplyr::if_else(is.null(day) == TRUE, "", as.character(day)),
-    blockid = rlang::eval_tidy(.blockid)
+  pref <- df_target_station$prec_no
+  station_type <-
+    dplyr::if_else(df_target_station$station_type == "官", "s", "a")
+
+  station_type <-
+    dplyr::if_else(selected_item == "annually",
+                   station_type,
+                   paste0(station_type, "1"))
+
+  list(
+    url = glue::glue(
+      "http://www.data.jma.go.jp/obd/stats/etrn/view/{selected_item}_{station_type}.php?prec_no={pref}&block_no={blockid}&year={year}&month={month}&day={day}&view=",
+      day = dplyr::if_else(is.null(day) == TRUE, "", day),
+      blockid = rlang::eval_tidy(.blockid)
+    ),
+    station_type = station_type
   )
+
+
 }
 
 convert_variable_unit <- function(.data) {
