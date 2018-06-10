ecd94bd1d0ead91cd23cf39d13122b66ef16fb4c
Shinya Uryu <uryu.shinya@nies.go.jp>
2018-06-10 17:04:45 +0900

    Add station data docs

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' JMA's Stations list
+#'
+#' @description XXX
+#' @format
+#' \itemize{
+#'   \item{area}
+#'   \item{station_no}
+#'   \item{station_type}
+#'   \item{station_name}
+#'   \item{address}
+#'   \item{elevation}
+#'   \item{observation_begin}
+#'   \item{note1}
+#'   \item{note1}
+#'   \item{note2}
+#'   \item{katakana}
+#'   \item{prec_no}
+#'   \item{block_no}
+#'   \item{geometry}
+#' }
+"stations"
