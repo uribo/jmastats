db68b94f7806d98b27eab0025b5bae0b0fb214da
Shinya Uryu <suika1127@gmail.com>
2023-08-18 11:38:05 +0900

    [add]キャッシュファイルの削除関数

--- R/appdir.R
+++ R/appdir.R
@@ -8,6 +8,19 @@ reset_cache <- function() {
          recursive = TRUE)
 }
 
+pick_out_cache <- function(item = NULL,
+                           block_no, year, month, day) {
+  target <-
+    detect_target(item, block_no, year, month, day)
+  param <-
+    xml2::url_parse(target$url)$query
+  file_loc <-
+    search_cache_file(item, target$station_type, param)
+  if (file.exists(file_loc)) {
+    unlink(file_loc)
+  }
+}
+
 search_cache_file <- function(item, station_type, param) {
   cache_dir <- rappdirs::user_cache_dir("jmastats")
   if (!file.exists(cache_dir)) {
